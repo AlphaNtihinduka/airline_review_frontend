c2d14ca0c765ca57f059464360be3d3e1d560a52
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2023-02-14 02:20:56 +0300

    Fetch airline api in browser

--- src/components/Airlines.js
+++ src/components/Airlines.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
+import axios from "axios";
 
 const Airlines = () => {
-    return <div>These are the airlines</div>
+    const [airlines, setAirlines] = useState([])
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:3000/api/v1/airlines')
+        .then(resp => console.log(resp))
+        .catch( resp => console.log(resp))
+    }, [airlines.length])
+    return (<div>These are the airlines</div>)
 }
 
 export default Airlines
